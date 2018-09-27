6abccd54aa92bbb5a70d955df1348c0bea6cf0d3
Jeffrey Jackovich <JeffreyJackovich@users.noreply.github.com>
2018-09-27 19:22:02 -0400

    removed quotes from the port variable

--- lesson2/topic_d/csv_to_mysql__doc_topics.py
+++ lesson2/topic_d/csv_to_mysql__doc_topics.py
@@ -5,7 +5,7 @@ row_list = []
 #mysql
 conn = pymysql.connect(db='<your db name>',  # eg. linkedTopicDB2
 						host='<your host name > ', # eg. 'linkedtopicsdbinstance2.hslfhsmwlcww.us-east-1.rds.amazonaws.com'
-						port='<port number>',
+						port= <port number>, # eg. 3306
 						user='administrator',
 						password='<your password>',
 						use_unicode=True,
