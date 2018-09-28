7814cabd9709b93abe168170a2bf404b92fbc13d
Jeffrey Jackovich <JeffreyJackovich@users.noreply.github.com>
2018-09-27 20:10:58 -0400

    Delete known_structure_analysis.sql

--- lesson3/topic_a/known_structure_analysis.sql
+++
@@ -1,9 +0,0 @@
-SELECT tt.topic, 
-	tt.term, 
-	tt.weight, 
-	dt.docname,
-    dt.proportion
-FROM linkedTopicDB2.known_structure_topic_terms tt, linkedTopicDB2.known_structure_doc_topics dt
-WHERE tt.topic = dt.topic
-ORDER BY docname ASC;
- 
